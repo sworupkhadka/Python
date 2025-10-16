91c8ca02f730b24f4603b85a0cb9936b8462c853
sworup <sworupkhadka1323@gmail.com>
2025-10-16 16:01:25 +0545

    day 16 final

---
+++ day 16/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+timmy=Turtle()
+print(timmy)
+
+timmy.shape("turtle")
+timmy.color("red")
+timmy.forward(100)
+
+
+my_screen= Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+from tabletest import PrettyTable
+print("PrettyTable installed successfully!")
